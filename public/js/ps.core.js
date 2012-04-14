e42a5358257c8bb613056f4e16f9d2c457ee0b1b
Casey Thomas <cpthomas@gmail.com>
2012-04-14 16:16:02 -0400

    Added function to retrieve parent elements from HAR

--- public/js/ps.core.js
+++ public/js/ps.core.js
@@ -7,14 +7,36 @@ PS.core = (function ($) {
 
   _self = {
 
-    getHar: function() {
+    getHar: function(url) {
       $.ajax({
         dataType: 'json',
         url: PS.config.ajaxPrefix + '/news.yahoo.com.json',
         success: function(data){
-          console.log(data);
+          _self.getParents(data);
         }
       });
+    },
+
+    getParents: function(data) {
+      var parents,
+        parentIds,
+        cleanParents;
+
+      cleanParents = [];
+      
+      parentIds = _.pluck(data.log.entries, 'parentid');
+      parents = _.uniq(parentIds);
+      _.each(parents, function(num){
+        if (num !== null) {
+          cleanParents.push(num);
+        }
+      });
+
+      console.log(cleanParents);
+    },
+
+    returnHar: function(data) {
+      return data;
     }
 
   };
